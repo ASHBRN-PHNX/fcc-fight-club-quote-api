904370b393b27ef6469a52d5202cb1e3ffc975ea
Alisdair Frost <alisdair.frost@gmail.com>
2016-07-26 12:20:10 -0600

    js include to html
    Javascript running in HTML

--- quote.js
+++ quote.js
@@ -1,3 +1,7 @@
+
+
 var wikiQuote = (function () {
-    var API_URL = "http://en.wikiquote.org/w/api.php";
+    // This file provides the base function to randomly generate a fight club quote from wikiQuote
+    
+    var API_URL = "http://en.wikiquote.org//w/api.php?action=parse&format=json&page=Fight_Club_(film)&prop=text&section=2";
 });
